c0f2aa3f8b198aa5339eb9ff777cf37972b2d76b
AffogatoLabs <AffogatoLabs@protonmail.com>
2021-02-16 02:54:11 -0500

    Relative sizing and blue bg

--- frontend/src/components/Swap/index.js
+++ frontend/src/components/Swap/index.js
@@ -16,14 +16,14 @@ const Swap = (props) => {
   
     <Paper
         style={{
-            maxWidth: 720,
-            minWidth: 360,
+            maxWidth: "40em",
+            minWidth: "30em",
             paddingTop: 2,
             paddingBottom: 2,
             paddingRight: 2,
             paddingLeft:2,
             backgroundColor: "grey",
-            marginTop: 200,
+            marginTop: "5em",
             marginBottom: 200,
             marginLeft: "auto",
             marginRight: "auto",
@@ -32,12 +32,12 @@ const Swap = (props) => {
         }}>
     <Paper
         style={{
-            maxWidth: 720,
-            minWidth: 360,
-            paddingTop: 8,
+            maxWidth: "40em",
+            minWidth: "30em",
+            paddingTop: 12,
             paddingBottom: 20,
-            paddingRight: 8,
-            paddingLeft:8,
+            paddingRight: 12,
+            paddingLeft: 12,
             backgroundColor: "black",
             marginLeft: "auto",
             marginRight: "auto",
@@ -47,10 +47,10 @@ const Swap = (props) => {
     <Paper
       elevation={4}
       style={{
-        maxWidth: 720,
-        minWidth: 360,
+        maxWidth: "40em",
+        minWidth: "30em",
         padding: 20,
-        backgroundColor: themeContext["swapBackground"],
+        backgroundColor: "#8aa1ff",
         marginLeft: "auto",
         marginRight: "auto",
         borderRadius: 50,
